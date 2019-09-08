99e492f5819cb35500362a7af82e3057a714b7ef
Immanuela Belaineh <belainehi@berea.edu>
2019-09-08 19:46:26 -0400

    Just created two new objects. One is main the other is square.

--- project.py
+++ project.py
@@ -1,10 +1,17 @@
 import turtle
 
-def make_square(side):
+def make_window (name):
+    "'This creates the window.'"
 
 
-for side in range(2):
-    shape.forward(100)
-    shape.right(90)
-    shape.forward(140)
-    shape.right(90)
+
+def main ():
+    "'This makes the square and the window."''
+    wn = turtle.Screen()  # Makes a new turtle screen
+    shape = turtle.Turtle()
+    for side in range(2):
+        side.forward(100)
+        side.right(90)
+        side.forward(100)
+        side.right(90)
+    return
