ec5cd22e4cc23a2444103606415c3a0a8559f749
Immanuela Belaineh <belainehi@berea.edu>
2019-09-11 10:36:09 -0400

    Just created two new objects. One is main the other is square.

--- project.py
+++ project.py
@@ -1,43 +1,49 @@
+#
+#
 import turtle
 
 
-def main():
-    "'This makes the square and the window."''
-    wn = turtle.Screen()  # Makes a new turtle screen
-    wn.bgcolor("white")
-    side = turtle.Turtle()
-    side.penup()
-    side.setpos(-200,200)
-    side.pendown()
-    side.pensize(30)
+def boustrophedon(g):
+    g.penup()
+    g.setpos(-170, 170)
+    g.pendown()
+    for w in range(30):
+        g.color("yellow")
+        g.speed(10)
+        g.right(90)
+        # side.forward(5)
+        g.left(90)
+        g.forward(440)
+        g.right(90)
+        g.forward(5)
+        g.right(90)
+        g.forward(440)
+        g.left(90)
+        g.forward(5)
+        g.left(90)
+def square (side):
+    # draw square
     for i in range(2):
         side.forward(500)
         side.right(90)
         side.forward(500)
         side.right(90)
 
-    for w in range(50):
-
-        side.forward(5)
-        side.right(90)
-        side.forward(5)
-        side.left(90)
-        side.forward(495)
-        side.right(90)
-        side.forward(5)
-        side.right(90)
-        side.forward(495)
-        side.left(90)
-        side.forward(5)
-        side.left(90)
-
-
-
-
-
-
+def main():
+    "'This makes the square and the window. It also draws the insides by using a second for loop."''
+    #Create a window
+    wn = turtle.Screen()
+    wn.bgcolor("white")
+    #Create a turtle
+    side = turtle.Turtle()
+    side.penup()
+    side.setpos(-200,200)
+    side.pendown()
+    side.pensize(30)
+    #draw the insides
+    square (side)
+    boustrophedon (side)
     wn.exitonclick()
-
 
 main()
 
